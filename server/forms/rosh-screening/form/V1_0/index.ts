4b90f826890e5ccd38a91f76a7398c8558882b54
JakeGoodwin22 <103187669+JakeGoodwin22@users.noreply.github.com>
2025-09-04 12:42:53 +0100

    moved journey to parent

--- server/forms/rosh-screening/form/V1_0/index.ts
+++ server/forms/rosh-screening/form/V1_0/index.ts
@@ -1,10 +1,30 @@
-import { journey } from '@form-engine/form/builders'
-import riskToOthers from './risk-to-others'
+import { journey, Post, step, submitTransition } from '@form-engine/form/builders'
+import { Condition } from '@form-engine/registry/conditions'
+import * as riskToOthersIndex from './risk-to-others/index'
 
 export const roshScreening = journey({
   code: 'rosh_screening',
   title: 'ROSH screening',
   path: '/rosh-screening',
   version: '1.0',
-  children: [riskToOthers],
+  steps: [
+    step({
+      path: riskToOthersIndex.URL.riskToOthers,
+      blocks: [...riskToOthersIndex.riskToOthersTypeGroup, riskToOthersIndex.continueButton],
+      onSubmission: [
+        submitTransition({
+          when: Post('action').match(Condition.Equals('continue')),
+          validate: true,
+          onValid: {
+            // TODO: Save data
+            next: [{ goto: '/rosh-screening/risk-to-children' }], // TODO: Update to correct URL when page exists
+          },
+          onInvalid: {
+            // TODO: Save data
+            next: [{ goto: '@self' }],
+          },
+        }),
+      ],
+    }),
+  ],
 })
