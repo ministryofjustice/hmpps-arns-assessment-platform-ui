08025e5c33161ae708b59eea697ab15ab62499ba
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-11-27 19:35:39 +0000

    Add ThunkHandlerRegistry for storing handlers

--- packages/form-engine/src/core/ast/compilation/CompilationDependencies.ts
+++ packages/form-engine/src/core/ast/compilation/CompilationDependencies.ts
@@ -3,6 +3,7 @@ import { NodeFactory } from '@form-engine/core/ast/nodes/NodeFactory'
 import NodeRegistry from '@form-engine/core/ast/registration/NodeRegistry'
 import MetadataRegistry from '@form-engine/core/ast/registration/MetadataRegistry'
 import { PseudoNodeFactory } from '@form-engine/core/ast/nodes/PseudoNodeFactory'
+import ThunkHandlerRegistry from '@form-engine/core/ast/thunks/registries/ThunkHandlerRegistry'
 import DependencyGraph from '@form-engine/core/ast/dependencies/DependencyGraph'
 
 export class CompilationDependencies {
@@ -12,6 +13,7 @@ export class CompilationDependencies {
     readonly pseudoNodeFactory = new PseudoNodeFactory(nodeIdGenerator, NodeIDCategory.COMPILE_PSEUDO),
     readonly nodeRegistry: NodeRegistry = new NodeRegistry(),
     readonly metadataRegistry: MetadataRegistry = new MetadataRegistry(),
+    readonly thunkHandlerRegistry = new ThunkHandlerRegistry(),
     readonly dependencyGraph: DependencyGraph = new DependencyGraph(),
   ) {}
 
@@ -21,6 +23,7 @@ export class CompilationDependencies {
     const clonedPseudoNodeFactory = new PseudoNodeFactory(clonedNodeIdGenerator, NodeIDCategory.COMPILE_PSEUDO)
     const clonedNodeRegistry = this.nodeRegistry.clone()
     const clonedMetadataRegistry = this.metadataRegistry.clone()
+    const clonedThunkHandlerRegistry = this.thunkHandlerRegistry.clone()
     const clonedDependencyGraph = this.dependencyGraph.clone()
 
     return new CompilationDependencies(
@@ -29,6 +32,7 @@ export class CompilationDependencies {
       clonedPseudoNodeFactory,
       clonedNodeRegistry,
       clonedMetadataRegistry,
+      clonedThunkHandlerRegistry,
       clonedDependencyGraph,
     )
   }
