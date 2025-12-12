c8f5c6d3e2e12ac0918f0ba23b87d1d2b79bd5aa
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-12-12 15:27:40 +0000

    Add new code-block component, add `copy-button` and use for code-block and error page (so useful!)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,10 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 import { SupportWidget } from './support-widget.mjs'
+import { CopyCode } from './copy-code.mjs'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
 
+customElements.define('app-copy-code', CopyCode)
 customElements.define('app-support-widget', SupportWidget)
--- packages/form-engine/src/registry/components/index.ts
+++ packages/form-engine/src/registry/components/index.ts
@@ -1,5 +1,6 @@
 import { html } from '@form-engine/registry/components/html'
 import { collectionBlock } from '@form-engine/registry/components/collectionBlock'
 import { templateWrapper } from '@form-engine/registry/components/templateWrapper'
+import { codeBlock } from '@form-engine/registry/components/codeBlock'
 
-export const coreComponents = [html, collectionBlock, templateWrapper]
+export const coreComponents = [html, collectionBlock, templateWrapper, codeBlock]
