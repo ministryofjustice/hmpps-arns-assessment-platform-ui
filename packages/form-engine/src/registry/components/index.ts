3d921993dccfb1842c5f5b202b3e5336fbccca85
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-12-12 15:17:39 +0000

    Add `TemplateWrapper` component for doing some crazy layout fudgery

--- packages/form-engine/src/registry/components/index.ts
+++ packages/form-engine/src/registry/components/index.ts
@@ -1,4 +1,5 @@
 import { html } from '@form-engine/registry/components/html'
 import { collectionBlock } from '@form-engine/registry/components/collectionBlock'
+import { templateWrapper } from '@form-engine/registry/components/templateWrapper'
 
-export const coreComponents = [html, collectionBlock]
+export const coreComponents = [html, collectionBlock, templateWrapper]
