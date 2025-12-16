932841ea5cfb1575e0209582230c03fa2df9f284
NicolaWard-SBG <nicola.ward@justice.gov.uk>
2025-12-16 12:23:27 +0000

    SP2-1725: WIP Create a goal page
    # Conflicts:
    #	docker/docker-compose.local.yml
    #	server/forms/sentence-plan/v1.0/journeys/goal-management/add-goal/fields.ts
    #	server/forms/sentence-plan/v1.0/journeys/goal-management/add-goal/step.ts
    #	server/forms/sentence-plan/v1.0/journeys/plan-overview/steps/plan/step.ts

--- server/forms/sentence-plan/v1.0/journeys/plan-overview/steps/plan/fields.ts
+++ server/forms/sentence-plan/v1.0/journeys/plan-overview/steps/plan/fields.ts
@@ -47,3 +47,12 @@ export const futureGoalsContent = block<HtmlBlock>({
     Data('caseData.name.forename'),
   ),
 })
+
+export const blankPlanOverviewContent = block<HtmlBlock>({
+  variant: 'html',
+  content: `<p class="govuk-!-display-none-print"> Name does not have any goals to work on now.</p>
+  <ul class="govuk-!-display-none-print">
+  <li><a href="/forms/sentence-plan/v1.0/crn/XYZ12345/goal/uuid/add-goal/areaOfNeed">Create a goal with NAME</a></li>
+  <li><a href="/about" >View Information from NAMES assessment</a></li>
+  </ul>`,
+})
