6ff668d0d0fee0229f84a347c2080dbe43dfaf7a
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2025-12-30 12:50:41 +0000

    SP2-1783: Add locals boolean for 'Plan history' tab

--- server/forms/sentence-plan/v1.0/steps/mpop-access/step.ts
+++ server/forms/sentence-plan/v1.0/steps/mpop-access/step.ts
@@ -1,4 +1,4 @@
-import { accessTransition, Data, loadTransition, next, step } from '@form-engine/form/builders'
+import { accessTransition, Data, Format, loadTransition, next, step } from '@form-engine/form/builders'
 import { Condition } from '@form-engine/registry/conditions'
 import { SentencePlanV1Effects } from '../../effects'
 
@@ -6,6 +6,14 @@ export const mpopAccessStep = step({
   path: '/crn/:crn',
   title: 'MPOP Access',
   isEntryPoint: true,
+  view: {
+    locals: {
+      showPlanHistoryTab: Data('assessment.properties.AGREEMENT_STATUS.value').match(
+        Condition.Array.IsIn(['AGREED', 'COULD_NOT_ANSWER', 'DO_NOT_AGREE']),
+      ),
+    },
+  },
+
   onLoad: [
     loadTransition({
       effects: [SentencePlanV1Effects.loadOrCreatePlanByCrn(), SentencePlanV1Effects.setSessionAccessType('mpop')],
--- server/forms/sentence-plan/v1.0/steps/oasys-access/step.ts
+++ server/forms/sentence-plan/v1.0/steps/oasys-access/step.ts
@@ -6,6 +6,13 @@ export const oasysAccessStep = step({
   path: '/oasys',
   title: 'OASys Access',
   isEntryPoint: true,
+  view: {
+    locals: {
+      showPlanHistoryTab: Data('assessment.properties.AGREEMENT_STATUS.value').match(
+        Condition.Array.IsIn(['AGREED', 'COULD_NOT_ANSWER', 'DO_NOT_AGREE']),
+      ),
+    },
+  },
 
   onLoad: [
     loadTransition({
