8d2d65dc1e1ffcdd2eb5283275282cad90881926
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2025-12-30 10:56:26 +0000

    Add `goal-summary-card` / GoalSummaryCardDraft / GoalSummaryCardAgree

--- server/forms/sentence-plan/components/index.ts
+++ server/forms/sentence-plan/components/index.ts
@@ -1,4 +1,7 @@
 import { accessibleAutocomplete } from './accessible-autocomplete/accessibleAutocomplete'
+import { goalSummaryCardAgreed, goalSummaryCardDraft } from './goal-summary-card/goalSummaryCard'
 
 export type { AccessibleAutocomplete } from './accessible-autocomplete/accessibleAutocomplete'
-export const sentencePlanComponents = [accessibleAutocomplete]
+export { GoalSummaryCardAgreed, GoalSummaryCardDraft } from './goal-summary-card/goalSummaryCard'
+
+export const sentencePlanComponents = [accessibleAutocomplete, goalSummaryCardDraft, goalSummaryCardAgreed]
