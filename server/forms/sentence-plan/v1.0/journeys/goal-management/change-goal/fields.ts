ce57ef346a878f512cbf8c761fd2627f8448c91d
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2025-12-31 15:15:32 +0000

    SP2-1727: Add steps and fields for change-goal

--- server/forms/sentence-plan/v1.0/journeys/goal-management/change-goal/fields.ts
+++ server/forms/sentence-plan/v1.0/journeys/goal-management/change-goal/fields.ts
@@ -1,15 +1,65 @@
-import { block } from '@form-engine/form/builders'
+import { block, Data, Format, Self, validation } from '@form-engine/form/builders'
 import { HtmlBlock } from '@form-engine/registry/components/html'
 import { GovUKButton } from '@form-engine-govuk-components/components/button/govukButton'
+import { GovUKTextInput } from '@form-engine-govuk-components/components'
+import { Condition } from '@form-engine/registry/conditions'
+import { TemplateWrapper } from '@form-engine/registry/components/templateWrapper'
+import { AccessibleAutocomplete } from '../../../../components'
+import { isRelatedToOtherAreas, canStartNow } from '../shared-fields'
 
-export const pageHeading = block<HtmlBlock>({
-  variant: 'html',
-  content: '<h1 class="govuk-heading-l">Change Goal</h1>',
+const pageHeading = HtmlBlock({
+  content: Format(
+    `<span class="govuk-caption-l">%1</span>
+    <h1 class="govuk-heading-l">Change goal with %2</h1>`,
+    Data('activeGoal.areaOfNeedLabel'),
+    Data('caseData.name.forename'),
+  ),
 })
 
-export const continueButton = block<GovUKButton>({
-  variant: 'govukButton',
-  text: 'Save changes',
+const goalTitle = block<AccessibleAutocomplete>({
+  variant: 'accessibleAutocomplete',
+  data: Data('currentAreaOfNeed').path('goals'),
+  inputClasses: 'govuk-!-width-two-thirds',
+  field: GovUKTextInput({
+    code: 'goal_title',
+    label: {
+      text: Format('What goal should %1 try to achieve?', Data('caseData.name.forename')),
+      classes: 'govuk-label--m',
+    },
+    hint: 'Search for a suggested goal or enter your own. Add one goal at a time.',
+    validate: [
+      validation({
+        when: Self().not.match(Condition.IsRequired()),
+        message: 'Select or enter what goal they should try to achieve',
+      }),
+    ],
+  }),
+})
+
+const saveGoalButton = GovUKButton({
+  text: 'Save goal',
   name: 'action',
-  value: 'continue',
+  value: 'saveGoal',
+  preventDoubleClick: true,
+})
+
+const forename = Data('caseData.name.forename')
+
+export const pageLayout = TemplateWrapper({
+  template: `
+    <div>
+      {{slot:pageHeading}}
+      {{slot:goalTitle}}
+      {{slot:isRelatedToOtherAreas}}
+      {{slot:canStartNow}}
+      {{slot:saveGoalButton}}
+    </div>
+  `,
+  slots: {
+    pageHeading: [pageHeading],
+    goalTitle: [goalTitle],
+    isRelatedToOtherAreas: [isRelatedToOtherAreas],
+    canStartNow: [canStartNow(forename)],
+    saveGoalButton: [saveGoalButton],
+  },
 })
--- server/forms/sentence-plan/v1.0/journeys/goal-management/change-goal/step.ts
+++ server/forms/sentence-plan/v1.0/journeys/goal-management/change-goal/step.ts
@@ -1,17 +1,64 @@
-import { next, step, submitTransition } from '@form-engine/form/builders'
-import { pageHeading, continueButton } from './fields'
+import {
+  accessTransition,
+  Answer,
+  Data,
+  loadTransition,
+  next,
+  Post,
+  step,
+  submitTransition,
+} from '@form-engine/form/builders'
+import { Condition } from '@form-engine/registry/conditions'
+import { pageLayout } from './fields'
+import { SentencePlanV1Effects } from '../../../effects'
 
+/**
+ * Change Goal page
+ *
+ * Allows users to edit an existing goal's details.
+ *
+ * When a goal is changed from "Goal for Now" (ACTIVE) to a future goal,
+ * it redirects to the Future Goals tab.
+ */
 export const changeGoalStep = step({
   path: '/change-goal',
   title: 'Change Goal',
   isEntryPoint: true,
-  blocks: [pageHeading, continueButton],
+
+  blocks: [pageLayout],
+
+  onLoad: [
+    loadTransition({
+      effects: [
+        SentencePlanV1Effects.deriveGoalsWithStepsFromAssessment(),
+        SentencePlanV1Effects.loadActiveGoalForEdit(),
+      ],
+    }),
+  ],
+
+  onAccess: [
+    // If goal not found, redirect to plan overview
+    accessTransition({
+      guards: Data('activeGoal').not.match(Condition.IsRequired()),
+      redirect: [next({ goto: '../../plan/overview' })],
+    }),
+  ],
+
   onSubmission: [
     submitTransition({
+      when: Post('action').match(Condition.Equals('saveGoal')),
       validate: true,
       onValid: {
-        // TODO: Save goal changes and return to plan overview
-        next: [next({ goto: '/plan-overview/plan' })],
+        effects: [SentencePlanV1Effects.updateActiveGoal()],
+        next: [
+          // If changed to a future goal, redirect to future goals tab
+          next({
+            when: Answer('can_start_now').match(Condition.Equals('no')),
+            goto: '../../plan/overview?type=future',
+          }),
+          // Otherwise redirect to current goals tab
+          next({ goto: '../../plan/overview?type=current' }),
+        ],
       },
     }),
   ],
