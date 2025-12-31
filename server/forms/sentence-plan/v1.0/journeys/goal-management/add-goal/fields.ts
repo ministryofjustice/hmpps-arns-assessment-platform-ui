7f9bdfe0a8d550769448a6edcf6bc8fd8c9c7ed8
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2025-12-31 15:14:12 +0000

    SP2-1727: Extract reusable blocks from add-goal fields

--- server/forms/sentence-plan/v1.0/journeys/goal-management/add-goal/fields.ts
+++ server/forms/sentence-plan/v1.0/journeys/goal-management/add-goal/fields.ts
@@ -1,13 +1,12 @@
-import { and, Answer, block, Data, Format, Item, Iterator, Params, Self, validation } from '@form-engine/form/builders'
+import { block, Data, Format, Item, Iterator, Params, Self, validation } from '@form-engine/form/builders'
 import { HtmlBlock } from '@form-engine/registry/components/html'
 import { GovUKButton } from '@form-engine-govuk-components/components/button/govukButton'
-import { GovUKRadioInput, GovUKTextInput, GovUKCheckboxInput } from '@form-engine-govuk-components/components'
+import { GovUKTextInput } from '@form-engine-govuk-components/components'
 import { Condition } from '@form-engine/registry/conditions'
-import { MOJDatePicker, MOJSideNavigation } from '@form-engine-moj-components/components'
+import { MOJSideNavigation } from '@form-engine-moj-components/components'
 import { TemplateWrapper } from '@form-engine/registry/components/templateWrapper'
-import { Transformer } from '@form-engine/registry/transformers'
-import { Generator } from '@form-engine/registry/generators'
 import { AccessibleAutocomplete } from '../../../../components'
+import { isRelatedToOtherAreas, canStartNow } from '../shared-fields'
 
 // Side navigation for areas of need
 const sideNavigation = MOJSideNavigation({
@@ -50,143 +49,6 @@ const goalTitle = block<AccessibleAutocomplete>({
   }),
 })
 
-const relatedAreasOfNeed = GovUKCheckboxInput({
-  code: 'related_areas_of_need',
-  multiple: true,
-  hint: {
-    text: 'Select all that apply.',
-  },
-  fieldset: {
-    legend: {
-      classes: 'govuk-fieldset__legend--m',
-    },
-  },
-  items: Data('otherAreasOfNeed').each(
-    Iterator.Map({
-      value: Item().path('slug'),
-      text: Item().path('text'),
-    }),
-  ),
-  validate: [
-    validation({
-      when: Self().not.match(Condition.IsRequired()),
-      message: 'Select all related areas',
-    }),
-  ],
-  dependent: Answer('is_related_to_other_areas').match(Condition.Equals('yes')),
-})
-
-const isRelatedToOtherAreas = GovUKRadioInput({
-  code: 'is_related_to_other_areas',
-  fieldset: {
-    legend: {
-      text: 'Is this goal related to any other area of need?',
-      classes: 'govuk-fieldset__legend--m',
-    },
-  },
-  items: [
-    { value: 'yes', text: 'Yes', block: relatedAreasOfNeed },
-    { value: 'no', text: 'No' },
-  ],
-  validate: [
-    validation({
-      when: Self().not.match(Condition.IsRequired()),
-      message: 'Select yes if this goal is related to any other area of need',
-    }),
-  ],
-})
-
-// MOJ Date Picker uses DD/MM/YYYY format, so everything needs
-// converting into that format.
-const customTargetDate = MOJDatePicker({
-  code: 'custom_target_date',
-  label: {
-    text: 'Select a date',
-    classes: 'govuk-fieldset__legend--s',
-  },
-  hint: 'For example, 31/3/2023.',
-  // Set a minimum date of today in the DD/MM/YYYY format
-  minDate: Generator.Date.Today().pipe(Transformer.Date.Format('DD/MM/YYYY')),
-  formatters: [Transformer.String.ToISODate()],
-  validate: [
-    validation({
-      when: Self().not.match(Condition.IsRequired()),
-      message: 'Please enter a date',
-    }),
-    validation({
-      when: Self().not.match(Condition.Date.IsValid()),
-      message: 'Please enter a valid date',
-    }),
-    validation({
-      when: and(Self().not.match(Condition.Date.IsToday()), Self().not.match(Condition.Date.IsFutureDate())),
-      message: 'Date must be today or in the future',
-    }),
-  ],
-  dependent: Answer('target_date_option').match(Condition.Equals('set_another_date')),
-})
-
-const targetDateOption = GovUKRadioInput({
-  code: 'target_date_option',
-  fieldset: {
-    legend: {
-      text: Format('When should %1 aim to achieve this goal?', Data('caseData.name.forename')),
-      classes: 'govuk-fieldset__legend--m',
-    },
-  },
-  items: [
-    {
-      value: 'date_in_3_months',
-      text: Format(
-        'In 3 months (%1)',
-        Generator.Date.Today().pipe(Transformer.Date.AddMonths(3), Transformer.Date.ToUKLongDate()),
-      ),
-    },
-    {
-      value: 'date_in_6_months',
-      text: Format(
-        'In 6 months (%1)',
-        Generator.Date.Today().pipe(Transformer.Date.AddMonths(6), Transformer.Date.ToUKLongDate()),
-      ),
-    },
-    {
-      value: 'date_in_12_months',
-      text: Format(
-        'In 12 months (%1)',
-        Generator.Date.Today().pipe(Transformer.Date.AddMonths(12), Transformer.Date.ToUKLongDate()),
-      ),
-    },
-    { divider: 'or' },
-    { value: 'set_another_date', text: 'Set another date', block: customTargetDate },
-  ],
-  validate: [
-    validation({
-      when: Self().not.match(Condition.IsRequired()),
-      message: 'Select when they should aim to achieve this goal',
-    }),
-  ],
-  dependent: Answer('can_start_now').match(Condition.Equals('yes')),
-})
-
-const canStartNow = GovUKRadioInput({
-  code: 'can_start_now',
-  fieldset: {
-    legend: {
-      text: Format('Can %1 start working on this goal now?', Data('caseData.name.forename')),
-      classes: 'govuk-fieldset__legend--m',
-    },
-  },
-  items: [
-    { value: 'yes', text: 'Yes', block: targetDateOption },
-    { value: 'no', text: 'No, it is a future goal' },
-  ],
-  validate: [
-    validation({
-      when: Self().not.match(Condition.IsRequired()),
-      message: 'Select yes if they can start working on this goal now',
-    }),
-  ],
-})
-
 const addStepsButton = GovUKButton({
   text: 'Add Steps',
   name: 'action',
@@ -216,6 +78,8 @@ const buttonGroup = TemplateWrapper({
   },
 })
 
+const forename = Data('caseData.name.forename')
+
 // Two-column layout wrapper
 export const twoColumnLayout = (): TemplateWrapper => {
   return TemplateWrapper({
@@ -232,7 +96,7 @@ export const twoColumnLayout = (): TemplateWrapper => {
     `,
     slots: {
       sideNav: [sideNavigation],
-      content: [pageHeading, goalTitle, isRelatedToOtherAreas, canStartNow, buttonGroup],
+      content: [pageHeading, goalTitle, isRelatedToOtherAreas, canStartNow(forename), buttonGroup],
     },
   })
 }
