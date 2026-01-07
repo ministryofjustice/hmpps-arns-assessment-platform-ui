d3a1ced578a9ae4b3e0255789e26e5e22cb79aaf
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2026-01-07 18:07:40 +0000

    SP2-1818: Replace references to server-side code in URLs for playwright testing

--- integration_tests/specs/sentence-plan/sentencePlanUtils.ts
+++ integration_tests/specs/sentence-plan/sentencePlanUtils.ts
@@ -1,18 +1,17 @@
 import { Page } from '@playwright/test'
-import { createGoalStep } from '../../../server/forms/sentence-plan/v1.0/journeys/goal-management/add-goal/step'
-import { sentencePlanV1Journey } from '../../../server/forms/sentence-plan/v1.0'
-import { oasysAccessStep } from '../../../server/forms/sentence-plan/v1.0/steps/oasys-access/step'
-import { planOverviewJourney } from '../../../server/forms/sentence-plan/v1.0/journeys/plan-overview'
-import { planStep } from '../../../server/forms/sentence-plan/v1.0/journeys/plan-overview/steps/plan/step'
-import { goalManagementJourney } from '../../../server/forms/sentence-plan/v1.0/journeys/goal-management'
 
 // sentence plan V1 URLs for use in playwright testing suits:
-const sentencePlanV1 = `/forms${sentencePlanV1Journey.path}` // '/forms/sentence-plan/v1.0'
+const sentencePlanV1 = '/forms/sentence-plan/v1.0'
+const oasysAccessStepPath = '/oasys'
+const planOverviewJourneyPath = '/plan'
+const planStepPath = '/overview'
+const goalManagementJourneyPath = '/goal/:uuid'
+const createGoalStepPath = '/add-goal/:areaOfNeed'
 
 export const sentencePlanV1URLs = {
-  OASYS_ENTRY_POINT: sentencePlanV1 + oasysAccessStep.path, // '/forms/sentence-plan/v1.0' + '/oasys'
-  PLAN_OVERVIEW: sentencePlanV1 + planOverviewJourney.path + planStep.path, // '/forms/sentence-plan/v1.0' + '/plan' + '/overview'
-  CREATE_GOAL: sentencePlanV1 + goalManagementJourney.path + createGoalStep.path, // '/forms/sentence-plan/v1.0' + '/goal/:uuid' + '/add-goal/:areaOfNeed'
+  OASYS_ENTRY_POINT: sentencePlanV1 + oasysAccessStepPath, // '/forms/sentence-plan/v1.0' + '/oasys'
+  PLAN_OVERVIEW: sentencePlanV1 + planOverviewJourneyPath + planStepPath, // '/forms/sentence-plan/v1.0' + '/plan' + '/overview'
+  CREATE_GOAL: sentencePlanV1 + goalManagementJourneyPath + createGoalStepPath, // '/forms/sentence-plan/v1.0' + '/goal/:uuid' + '/add-goal/:areaOfNeed'
 }
 
 // TODO: add create assessment util function (currently these are stubs)
