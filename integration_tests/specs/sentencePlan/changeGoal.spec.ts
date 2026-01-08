f91feecaefbace7fe8d1b83c68af7b30eba0a3d4
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2026-01-08 11:04:50 +0000

    SP2-1818: Rename SP test folder; setup for changeGoal test

--- integration_tests/specs/sentencePlan/changeGoal.spec.ts
+++ integration_tests/specs/sentencePlan/changeGoal.spec.ts
@@ -1,28 +1,26 @@
 import { expect, test } from '@playwright/test'
-import { loginHandover } from '../../testUtils'
+import { loginAndNavigateToPlan, resetStubs } from '../../testUtils'
 import ChangeGoalPage from '../../pages/sentencePlan/changeGoalPage'
-import { createTestGoal, sentencePlanV1URLs } from './sentencePlanUtils'
+import aapApi, { createAssessmentWithCurrentGoals } from '../../mockApis/aapApi'
 
 test.describe('Change goal journey', () => {
-  test.beforeEach(async ({ page }) => {
-    await loginHandover(page)
-    // currently this is required to correctly navigate to plan through OASys path:
-    await page.goto(sentencePlanV1URLs.OASYS_ENTRY_POINT)
+  test.beforeEach(async () => {
+    await aapApi.stubSentencePlanApis(createAssessmentWithCurrentGoals(1))
+  })
 
+  test.afterEach(async () => {
+    await resetStubs()
   })
 
   test.describe('when a goal exists', () => {
     test.beforeEach(async ({ page }) => {
-      await createTestGoal(page)
+      await loginAndNavigateToPlan(page)
       await page.getByRole('link', { name: 'Change goal' }).click()
     })
 
     test('can access chang goal page directly', async ({ page }) => {
       const changeGoalPage = await ChangeGoalPage.verifyOnPage(page)
       expect(changeGoalPage).toBeTruthy()
-
     })
-
   })
-
 })
