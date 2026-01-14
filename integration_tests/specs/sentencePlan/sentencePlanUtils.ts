ab1dcc4e7dc53e4d1a070c232fbbfbf00dd91f81
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2026-01-14 15:05:24 +0000

    SP2-1742: Add e2e tests for confirm if achieved page; refactored constant URLs in plan utils to be used in tests

--- integration_tests/specs/sentencePlan/sentencePlanUtils.ts
+++ integration_tests/specs/sentencePlan/sentencePlanUtils.ts
@@ -2,18 +2,19 @@ import { expect, Page } from '@playwright/test'
 import { login } from '../../testUtils'
 
 // sentence plan V1 URLs for use in playwright testing suits:
-const sentencePlanV1 = '/forms/sentence-plan'
+const sentencePlanFormPath = '/forms/sentence-plan'
+const v1Path = '/v1.0'
 const oasysAccessStepPath = '/oasys'
+const crnAccessStepPath = '/crn'
 const planOverviewJourneyPath = '/plan'
 const planStepPath = '/overview'
-const goalManagementJourneyPath = '/goal/:uuid'
-const createGoalStepPath = '/add-goal/:areaOfNeed'
+const goalManagementJourneyPath = '/goal'
 
 export const sentencePlanV1URLs = {
-  OASYS_ENTRY_POINT: sentencePlanV1 + oasysAccessStepPath, // '/forms/sentence-plan' + '/oasys'
-  CRN_ENTRY_POINT: `${sentencePlanV1}/crn`, // '/forms/sentence-plan/crn/:crn'
-  PLAN_OVERVIEW: sentencePlanV1 + planOverviewJourneyPath + planStepPath, // '/forms/sentence-plan' + '/plan' + '/overview'
-  CREATE_GOAL: sentencePlanV1 + goalManagementJourneyPath + createGoalStepPath, // '/forms/sentence-plan/v1.0' + '/goal/:uuid' + '/add-goal/:areaOfNeed'
+  OASYS_ENTRY_POINT: sentencePlanFormPath + oasysAccessStepPath, // '/forms/sentence-plan' + '/oasys'
+  CRN_ENTRY_POINT: sentencePlanFormPath + crnAccessStepPath, // '/forms/sentence-plan/crn/:crn'
+  PLAN_OVERVIEW: sentencePlanFormPath + v1Path + planOverviewJourneyPath + planStepPath, // '/forms/sentence-plan' + '/v1.0' + '/plan' + '/overview'
+  GOAL_MANAGEMENT_ROOT_PATH: sentencePlanFormPath + v1Path + goalManagementJourneyPath, // '/forms/sentence-plan' + '/v1.0' + '/goal'
 }
 
 /** Logs in and navigates to a sentence plan by CRN. */
