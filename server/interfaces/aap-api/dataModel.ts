f3d957d74564e93db5eab9d607a8c97827f104f6
NicolaWard-SBG <nicola.ward@justice.gov.uk>
2026-02-05 12:46:18 +0000

    SP2-1924: Update TimelineItem interface to match API response

--- server/interfaces/aap-api/dataModel.ts
+++ server/interfaces/aap-api/dataModel.ts
@@ -44,9 +44,11 @@ export interface Collection {
 }
 
 export interface TimelineItem {
-  type: string
-  createdAt: string
+  event: string
+  timestamp: string
   data: Record<string, any>
+  customType?: string
+  customData?: Record<string, any>
   user?: {
     id: string
     name: string
