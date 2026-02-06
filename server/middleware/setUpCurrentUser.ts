5a85315e342f0dcdeb724e1279ef89c1613ff613
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-02-06 09:22:47 +0000

    Switch to using `@ministryofjustice/hmpps-azure-telemetry` (#306)

--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -2,7 +2,6 @@ import { jwtDecode } from 'jwt-decode'
 import express from 'express'
 import { convertToTitleCase } from '../utils/utils'
 import logger from '../../logger'
-import { telemetry } from '../utils/telemetry'
 
 export default function setUpCurrentUser() {
   const router = express.Router()
@@ -38,17 +37,6 @@ export default function setUpCurrentUser() {
         username: res.locals.user.username,
         displayName: convertToTitleCase(name),
       }
-
-      telemetry.helpers.enrichSpanWithUser({
-        id: res.locals.user.userId,
-        authSource: res.locals.user.authSource,
-        roles: res.locals.user.userRoles,
-      })
-
-      telemetry.addSpanEvent('UserSessionPopulated', {
-        authSource: res.locals.user.authSource || 'unknown',
-        hasRoles: roles.length > 0,
-      })
 
       return next()
     } catch (error) {
