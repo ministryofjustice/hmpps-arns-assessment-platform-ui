2ff55f913e6b64cfa0ea4e2a8789365620b77d05
JakeGoodwin22 <103187669+JakeGoodwin22@users.noreply.github.com>
2026-02-13 14:05:45 +0000

    Reverted some over-enginered changes

--- server/forms/sentence-plan/effects/goals/createGoal.ts
+++ server/forms/sentence-plan/effects/goals/createGoal.ts
@@ -1,7 +1,6 @@
 import { BadRequest } from 'http-errors'
 import { SentencePlanContext, SentencePlanEffectsDeps } from '../types'
 import { wrapAll } from '../../../../data/aap-api/wrappers'
-import { addPlanOverviewSuccessNotification, getPlanOwnerPossessive } from '../notifications/notificationUtils'
 import {
   getRequiredEffectContext,
   calculateTargetDate,
@@ -24,66 +23,56 @@ import {
  * - target_date_option: Target date option
  * - custom_target_date: Custom date (if set_another_date)
  */
-interface CreateGoalOptions {
-  addPlanOverviewNotification?: boolean
-}
+export const createGoal = (deps: SentencePlanEffectsDeps) => async (context: SentencePlanContext) => {
+  const { user, assessmentUuid } = getRequiredEffectContext(context, 'createGoal')
+  const areaOfNeedSlug = context.getRequestParam('areaOfNeed')
 
-export const createGoal =
-  (deps: SentencePlanEffectsDeps) => async (context: SentencePlanContext, options?: CreateGoalOptions) => {
-    const { user, assessmentUuid } = getRequiredEffectContext(context, 'createGoal')
-    const areaOfNeedSlug = context.getRequestParam('areaOfNeed')
+  if (!areaOfNeedSlug) {
+    throw new BadRequest('Area of need is required to create a goal')
+  }
 
-    if (!areaOfNeedSlug) {
-      throw new BadRequest('Area of need is required to create a goal')
-    }
+  // Get form answers
+  const goalTitle = context.getAnswer('goal_title')
+  const relatedAreas = context.getAnswer('related_areas_of_need') ?? []
+  const canStartNow = context.getAnswer('can_start_now')
+  const targetDateOption = context.getAnswer('target_date_option')
+  const customDate = context.getAnswer('custom_target_date')
 
-    // Get form answers
-    const goalTitle = context.getAnswer('goal_title')
-    const relatedAreas = context.getAnswer('related_areas_of_need') ?? []
-    const canStartNow = context.getAnswer('can_start_now')
-    const targetDateOption = context.getAnswer('target_date_option')
-    const customDate = context.getAnswer('custom_target_date')
+  // Calculate target date and status
+  const targetDate = calculateTargetDate(canStartNow, targetDateOption, customDate)
+  const status = determineGoalStatus(canStartNow)
 
-    // Calculate target date and status
-    const targetDate = calculateTargetDate(canStartNow, targetDateOption, customDate)
-    const status = determineGoalStatus(canStartNow)
+  // Get or create GOALS collection
+  let goalsCollectionUuid = context.getData('goalsCollectionUuid')
 
-    // Get or create GOALS collection
-    let goalsCollectionUuid = context.getData('goalsCollectionUuid')
-
-    if (!goalsCollectionUuid) {
-      const createResult = await deps.api.executeCommand({
-        type: 'CreateCollectionCommand',
-        name: 'GOALS',
-        assessmentUuid,
-        user,
-      })
-
-      goalsCollectionUuid = createResult.collectionUuid
-    }
-
-    const properties = buildGoalProperties(status)
-    const answers = buildGoalAnswers(goalTitle, areaOfNeedSlug, relatedAreas, targetDate)
-
-    // Create the goal
-    const addResult = await deps.api.executeCommand({
-      type: 'AddCollectionItemCommand',
-      collectionUuid: goalsCollectionUuid,
-      properties: wrapAll(properties),
-      answers: wrapAll(answers),
-      timeline: {
-        type: 'GOAL_CREATED',
-        data: {},
-      },
+  if (!goalsCollectionUuid) {
+    const createResult = await deps.api.executeCommand({
+      type: 'CreateCollectionCommand',
+      name: 'GOALS',
       assessmentUuid,
       user,
     })
 
-    // Store goal UUID for redirect to add-steps
-    context.setData('activeGoalUuid', addResult.collectionItemUuid)
-
-    if (options?.addPlanOverviewNotification) {
-      const planOwnerPossessive = getPlanOwnerPossessive(context)
-      await addPlanOverviewSuccessNotification(context, `You added a goal to ${planOwnerPossessive} plan`, 'Goal added')
-    }
+    goalsCollectionUuid = createResult.collectionUuid
   }
+
+  const properties = buildGoalProperties(status)
+  const answers = buildGoalAnswers(goalTitle, areaOfNeedSlug, relatedAreas, targetDate)
+
+  // Create the goal
+  const addResult = await deps.api.executeCommand({
+    type: 'AddCollectionItemCommand',
+    collectionUuid: goalsCollectionUuid,
+    properties: wrapAll(properties),
+    answers: wrapAll(answers),
+    timeline: {
+      type: 'GOAL_CREATED',
+      data: {},
+    },
+    assessmentUuid,
+    user,
+  })
+
+  // Store goal UUID for redirect to add-steps
+  context.setData('activeGoalUuid', addResult.collectionItemUuid)
+}
--- server/forms/sentence-plan/versions/v1.0/journeys/goal-management/add-goal/step.ts
+++ server/forms/sentence-plan/versions/v1.0/journeys/goal-management/add-goal/step.ts
@@ -14,6 +14,7 @@ import {
 import { Condition } from '@form-engine/registry/conditions'
 import { sideNav, contentBlocks } from './fields'
 import { SentencePlanEffects } from '../../../../../effects'
+import { CaseData } from '../../../constants'
 
 /**
  * For adding a new goal.
@@ -63,7 +64,15 @@ export const createGoalStep = step({
       when: Post('action').match(Condition.Equals('saveWithoutSteps')),
       validate: true,
       onValid: {
-        effects: [SentencePlanEffects.createGoal({ addPlanOverviewNotification: true })],
+        effects: [
+          SentencePlanEffects.createGoal(),
+          SentencePlanEffects.addNotification({
+            type: 'success',
+            title: 'Goal added',
+            message: Format('You added a goal to %1 plan', CaseData.ForenamePossessive),
+            target: 'plan-overview',
+          }),
+        ],
         next: [
           redirect({
             when: Query('type').match(Condition.IsRequired()),
