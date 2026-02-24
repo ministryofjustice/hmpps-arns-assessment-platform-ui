94abc9b3b6c3dbba1cc3de13079aaef92b5796be
devkokov <dm.kokov@gmail.com>
2026-02-24 10:00:57 +0000

    reduce boilerplate in loadHistoricPlan

--- server/forms/sentence-plan/effects/plan/loadHistoricPlan.ts
+++ server/forms/sentence-plan/effects/plan/loadHistoricPlan.ts
@@ -1,35 +1,21 @@
-import { InternalServerError, NotFound } from 'http-errors'
+import { NotFound } from 'http-errors'
 import { SentencePlanContext, SentencePlanEffectsDeps } from '../types'
 import { derivePlanAgreementsData } from './derivePlanAgreementsFromAssessment'
 import { deriveGoalsWithSteps } from '../goals/deriveGoalsWithStepsFromAssessment'
+import { getRequiredEffectContext } from '../goals/goalUtils'
 
 /**
  * Load a historic plan version using the plan uuid from context and timestamp from URL param.
  */
 export const loadHistoricPlan = (deps: SentencePlanEffectsDeps) => async (context: SentencePlanContext) => {
-  const assessmentUuid = context.getData('assessmentUuid')
-  const user = context.getState('user')
-  const session = context.getSession()
-  const sessionDetails = session.sessionDetails
-
-  if (!assessmentUuid) {
-    throw new InternalServerError('Assessment UUID is required to load a historic plan')
-  }
-
-  if (!user) {
-    throw new InternalServerError('User is required to load a historic plan')
-  }
-
-  if (!sessionDetails?.planIdentifier) {
-    throw new InternalServerError('Plan identifier is required to load a historic plan')
-  }
+  const { user, assessmentUuid } = getRequiredEffectContext(context, 'loadHistoricPlan')
 
   const pointInTime = new Date(Number(context.getRequestParam('timestamp')))
 
   const assessment = await deps.api.executeQuery({
     type: 'AssessmentVersionQuery',
     user,
-    assessmentIdentifier: sessionDetails.planIdentifier,
+    assessmentIdentifier: { type: 'UUID', uuid: assessmentUuid },
     timestamp: pointInTime.toISOString().replace('Z', '999Z'),
   })
 
