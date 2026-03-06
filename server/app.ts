2ff5c1d2a09bd348311ff56f0c93ef4397da6f2f
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-03-06 12:02:52 +0000

    Add `setUpPreviousPageTracking` middleware

--- server/app.ts
+++ server/app.ts
@@ -19,6 +19,7 @@ import setUpWebRequestParsing from './middleware/setupRequestParsing'
 import setUpWebSecurity from './middleware/setUpWebSecurity'
 import setUpWebSession from './middleware/setUpWebSession'
 import setUpPreferencesCookie from './middleware/setUpPreferencesCookie'
+import setUpPreviousPageTracking from './middleware/setUpPreviousPageTracking'
 
 import routes from './routes'
 import type { Services } from './services'
@@ -88,6 +89,7 @@ export default function createApp(services: Services): express.Application {
   app.use(authorisationMiddleware([], services.deliusApiClient))
   app.use(setUpCsrf())
   app.use(setUpCurrentUser())
+  app.use(setUpPreviousPageTracking())
 
   // Mount routes
   app.use(routes(services))
