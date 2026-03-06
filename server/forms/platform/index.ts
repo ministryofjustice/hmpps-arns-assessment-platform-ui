e73a41f7300bf3a28611a6b1cf65c40d8914a38b
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-03-06 12:21:34 +0000

    Rename

--- server/forms/platform/index.ts
+++ server/forms/platform/index.ts
@@ -1,5 +1,5 @@
 import { createFormPackage, journey } from '@form-engine/form/builders'
-import { accessibilityStep } from './steps/accessibility/step'
+import { accessibilityStep } from './steps/accessibility-policy/step'
 import { cookiesPolicyStep } from './steps/cookies-policy/step'
 import { privacyPolicyStep } from './steps/privacy-policy/step'
 
