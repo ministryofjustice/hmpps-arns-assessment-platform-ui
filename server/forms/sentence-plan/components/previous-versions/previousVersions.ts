aafb67579070b2d7d4b9f5bf8edd8bce81482872
Iana Bezrodnykh <Iana.Bezrodnykh@justice.gov.uk>
2026-03-19 17:05:49 +0000

    Revert "SP2-2016: Change  tag to 'Plan first agreed' on previous versions pag…"
    This reverts commit b38762fbff459a84418c98216f50eee4c3f0be27.

--- server/forms/sentence-plan/components/previous-versions/previousVersions.ts
+++ server/forms/sentence-plan/components/previous-versions/previousVersions.ts
@@ -55,7 +55,7 @@ function buildParams(block: EvaluatedBlock<PreviousVersions>) {
       },
       agreed: {
         statuses: ['AGREED'],
-        text: 'Plan first agreed',
+        text: 'Plan agreed',
         classes: 'govuk-tag--blue',
       },
     },
