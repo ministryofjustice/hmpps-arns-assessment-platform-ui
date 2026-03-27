36aba004ecebff2463a893a92776eb2079430c09
lewis prescott <lewis.prescott@justice.gov.uk>
2026-03-27 10:19:31 +0000

    test: mpop wait for load

--- integration_tests/testUtils.ts
+++ integration_tests/testUtils.ts
@@ -4,6 +4,7 @@ const authSignIn = async (page: Page) => {
   await page.locator('#username').fill('AUTH_TEST')
   await page.locator('#password').fill('password123456')
   await page.locator('#submit').click()
+  await page.waitForLoadState('domcontentloaded')
 }
 
 export const login = async (page: Page) => {
