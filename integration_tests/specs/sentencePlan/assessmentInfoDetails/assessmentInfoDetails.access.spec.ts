c26f2c5c7c6223c917d108494f201432b5d04541
JakeGoodwin22 <103187669+JakeGoodwin22@users.noreply.github.com>
2026-04-02 14:17:35 +0100

    Move navigateToPlanOverviewViaMpop to shared utils and use beforeEach in MPoP test blocks

--- integration_tests/specs/sentencePlan/assessmentInfoDetails/assessmentInfoDetails.access.spec.ts
+++ integration_tests/specs/sentencePlan/assessmentInfoDetails/assessmentInfoDetails.access.spec.ts
@@ -1,20 +1,9 @@
-import { expect, type Page } from '@playwright/test'
+import { expect } from '@playwright/test'
 import { test, TargetService } from '../../../support/fixtures'
 import CreateGoalPage from '../../../pages/sentencePlan/createGoalPage'
 import ChangeGoalPage from '../../../pages/sentencePlan/changeGoalPage'
 import AddStepsPage from '../../../pages/sentencePlan/addStepsPage'
-import PrivacyScreenPage from '../../../pages/sentencePlan/privacyScreenPage'
-import PlanOverviewPage from '../../../pages/sentencePlan/planOverviewPage'
-import { navigateToSentencePlan, getDatePlusDaysAsISO, sentencePlanV1URLs } from '../sentencePlanUtils'
-import { login } from '../../../testUtils'
-
-const navigateToPlanOverviewViaMpop = async (page: Page, crn: string): Promise<void> => {
-  await login(page)
-  await page.goto(`${sentencePlanV1URLs.CRN_ENTRY_POINT}/${crn}`)
-  const privacyScreenPage = await PrivacyScreenPage.verifyOnPage(page)
-  await privacyScreenPage.confirmAndContinue()
-  await PlanOverviewPage.verifyOnPage(page)
-}
+import { navigateToSentencePlan, navigateToPlanOverviewViaMpop, getDatePlusDaysAsISO } from '../sentencePlanUtils'
 
 test.describe('Assessment Info Details - Access by Assessment Type', () => {
   test.describe('SAN_SP assessment type (private beta)', () => {
@@ -156,63 +145,42 @@ test.describe('Assessment Info Details - Access by Assessment Type', () => {
   })
 
   test.describe('SAN_SP assessment type via MPoP access', () => {
-    test('hides assessment info details on create goal page', async ({ page, createSession }) => {
-      const { crn } = await createSession({
+    test.beforeEach(async ({ page, createSession, sentencePlanBuilder }) => {
+      const { crn, sentencePlanId } = await createSession({
         targetService: TargetService.SENTENCE_PLAN,
         assessmentType: 'SAN_SP',
       })
 
+      await sentencePlanBuilder
+        .extend(sentencePlanId)
+        .withGoals([
+          {
+            title: 'Test Goal',
+            areaOfNeed: 'accommodation',
+            status: 'ACTIVE',
+            targetDate: getDatePlusDaysAsISO(90),
+          },
+        ])
+        .save()
+
       await navigateToPlanOverviewViaMpop(page, crn)
+    })
+
+    test('hides assessment info details on create goal page', async ({ page }) => {
       await page.goto('/sentence-plan/v1.0/goal/new/add-goal/accommodation')
       const createGoalPage = await CreateGoalPage.verifyOnPage(page)
 
       await expect(createGoalPage.assessmentInfoDetails).not.toBeVisible()
     })
 
-    test('hides assessment info details on change goal page', async ({ page, createSession, sentencePlanBuilder }) => {
-      const { crn, sentencePlanId } = await createSession({
-        targetService: TargetService.SENTENCE_PLAN,
-        assessmentType: 'SAN_SP',
-      })
-
-      await sentencePlanBuilder
-        .extend(sentencePlanId)
-        .withGoals([
-          {
-            title: 'Test Goal',
-            areaOfNeed: 'accommodation',
-            status: 'ACTIVE',
-            targetDate: getDatePlusDaysAsISO(90),
-          },
-        ])
-        .save()
-
-      await navigateToPlanOverviewViaMpop(page, crn)
+    test('hides assessment info details on change goal page', async ({ page }) => {
       await page.getByRole('link', { name: 'Change goal' }).click()
 
       const changeGoalPage = await ChangeGoalPage.verifyOnPage(page)
       await expect(changeGoalPage.assessmentInfoDetails).not.toBeVisible()
     })
 
-    test('hides assessment info details on add steps page', async ({ page, createSession, sentencePlanBuilder }) => {
-      const { crn, sentencePlanId } = await createSession({
-        targetService: TargetService.SENTENCE_PLAN,
-        assessmentType: 'SAN_SP',
-      })
-
-      await sentencePlanBuilder
-        .extend(sentencePlanId)
-        .withGoals([
-          {
-            title: 'Test Goal',
-            areaOfNeed: 'accommodation',
-            status: 'ACTIVE',
-            targetDate: getDatePlusDaysAsISO(90),
-          },
-        ])
-        .save()
-
-      await navigateToPlanOverviewViaMpop(page, crn)
+    test('hides assessment info details on add steps page', async ({ page }) => {
       await page.getByRole('link', { name: 'Add steps' }).click()
 
       const addStepsPage = await AddStepsPage.verifyOnPage(page)
@@ -221,63 +189,42 @@ test.describe('Assessment Info Details - Access by Assessment Type', () => {
   })
 
   test.describe('SP assessment type via MPoP access (national rollout)', () => {
-    test('hides assessment info details on create goal page', async ({ page, createSession }) => {
-      const { crn } = await createSession({
+    test.beforeEach(async ({ page, createSession, sentencePlanBuilder }) => {
+      const { crn, sentencePlanId } = await createSession({
         targetService: TargetService.SENTENCE_PLAN,
         assessmentType: 'SP',
       })
 
+      await sentencePlanBuilder
+        .extend(sentencePlanId)
+        .withGoals([
+          {
+            title: 'Test Goal',
+            areaOfNeed: 'accommodation',
+            status: 'ACTIVE',
+            targetDate: getDatePlusDaysAsISO(90),
+          },
+        ])
+        .save()
+
       await navigateToPlanOverviewViaMpop(page, crn)
+    })
+
+    test('hides assessment info details on create goal page', async ({ page }) => {
       await page.goto('/sentence-plan/v1.0/goal/new/add-goal/accommodation')
       const createGoalPage = await CreateGoalPage.verifyOnPage(page)
 
       await expect(createGoalPage.assessmentInfoDetails).not.toBeVisible()
     })
 
-    test('hides assessment info details on change goal page', async ({ page, createSession, sentencePlanBuilder }) => {
-      const { crn, sentencePlanId } = await createSession({
-        targetService: TargetService.SENTENCE_PLAN,
-        assessmentType: 'SP',
-      })
-
-      await sentencePlanBuilder
-        .extend(sentencePlanId)
-        .withGoals([
-          {
-            title: 'Test Goal',
-            areaOfNeed: 'accommodation',
-            status: 'ACTIVE',
-            targetDate: getDatePlusDaysAsISO(90),
-          },
-        ])
-        .save()
-
-      await navigateToPlanOverviewViaMpop(page, crn)
+    test('hides assessment info details on change goal page', async ({ page }) => {
       await page.getByRole('link', { name: 'Change goal' }).click()
 
       const changeGoalPage = await ChangeGoalPage.verifyOnPage(page)
       await expect(changeGoalPage.assessmentInfoDetails).not.toBeVisible()
     })
 
-    test('hides assessment info details on add steps page', async ({ page, createSession, sentencePlanBuilder }) => {
-      const { crn, sentencePlanId } = await createSession({
-        targetService: TargetService.SENTENCE_PLAN,
-        assessmentType: 'SP',
-      })
-
-      await sentencePlanBuilder
-        .extend(sentencePlanId)
-        .withGoals([
-          {
-            title: 'Test Goal',
-            areaOfNeed: 'accommodation',
-            status: 'ACTIVE',
-            targetDate: getDatePlusDaysAsISO(90),
-          },
-        ])
-        .save()
-
-      await navigateToPlanOverviewViaMpop(page, crn)
+    test('hides assessment info details on add steps page', async ({ page }) => {
       await page.getByRole('link', { name: 'Add steps' }).click()
 
       const addStepsPage = await AddStepsPage.verifyOnPage(page)
