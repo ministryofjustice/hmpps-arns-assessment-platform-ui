68a346602b64701d5eec9b9dcb71f0911707414d
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2026-04-27 14:25:29 +0100

    Fix returnUrl in HandoverBuilder

--- integration_tests/builders/HandoverBuilder.ts
+++ integration_tests/builders/HandoverBuilder.ts
@@ -76,7 +76,7 @@ export class HandoverBuilderInstance {
     displayName: 'Test User',
     accessMode: 'READ_WRITE',
     planAccessMode: 'READ_WRITE',
-    returnUrl: 'http://localhost:7072',
+    returnUrl: process.env.BASE_URL || 'http://localhost:7072',
   }
 
   constructor(client: TestHandoverApiClient, association: CoordinatorAssociation) {
