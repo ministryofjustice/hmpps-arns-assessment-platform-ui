e3b6cd5609ed9745a21918e1789ef492edb14778
Nicola Ward <123090672+NicolaWard-SBG@users.noreply.github.com>
2026-06-25 13:31:26 +0100

    SP2-2289 Update goal and steps post agreement (#588)
    Co-authored-by: Jake Goodwin <103187669+JakeGoodwin22@users.noreply.github.com>

--- server/forms/sentence-plan/versions/v1.0/journeys/goal-management/update-goal-and-steps/step.ts
+++ server/forms/sentence-plan/versions/v1.0/journeys/goal-management/update-goal-and-steps/step.ts
@@ -11,9 +11,6 @@ import {
 } from '@ministryofjustice/hmpps-forge/core/authoring'
 import {
   pageHeading,
-  goalSubheading,
-  goalInfoFuture,
-  goalInfoActive,
   reviewStepsHeading,
   reviewStepsTable,
   addOrChangeStepsLink,
@@ -21,6 +18,7 @@ import {
   progressNotesSection,
   viewAllNotesSection,
   actionButtons,
+  goalContextInsetText,
 } from './fields'
 import { AuditEvent, SentencePlanEffects } from '../../../../../effects'
 import { redirectIfGoalNotFound, redirectIfNotPostAgreement } from '../../../guards'
@@ -42,9 +40,7 @@ export const updateGoalAndStepsStep = step({
   },
   blocks: [
     pageHeading,
-    goalSubheading,
-    goalInfoFuture,
-    goalInfoActive,
+    goalContextInsetText,
     reviewStepsHeading,
     reviewStepsTable,
     addOrChangeStepsLink,
