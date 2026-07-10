1dd4dfbf98be7077f19ea4e0362866b07961c0f9
Nicola Ward <123090672+NicolaWard-SBG@users.noreply.github.com>
2026-07-10 13:17:23 +0100

    SP2-2458: Add client side events to Update links (#603)

--- server/forms/sentence-plan/versions/v1.0/journeys/plan-overview/steps/plan/fields.ts
+++ server/forms/sentence-plan/versions/v1.0/journeys/plan-overview/steps/plan/fields.ts
@@ -252,6 +252,9 @@ export const goalsSection = TemplateWrapper({
                                 href: when(isAchievedGoal)
                                   .then(Format('../goal/%1/view-inactive-goal', Item().path('uuid')))
                                   .else(Format('../goal/%1/change-goal', Item().path('uuid'))),
+                                dataAiId: when(isAchievedGoal)
+                                  .then('view-inactive-goal-inline-link')
+                                  .else('update-draft-goal-inline-link'),
                               },
                               {
                                 text: 'Add or update steps',
@@ -330,6 +333,13 @@ export const goalsSection = TemplateWrapper({
                                 )
                                   .then(Format('../goal/%1/view-inactive-goal', Item().path('uuid')))
                                   .else(Format('../goal/%1/update-goal-steps', Item().path('uuid'))),
+                                dataAiId: when(
+                                  Item()
+                                    .path('status')
+                                    .match(Condition.Array.IsIn(['ACHIEVED', 'REMOVED'])),
+                                )
+                                  .then('view-inactive-goal-inline-link')
+                                  .else('update-goal-inline-link'),
                               },
                             ],
                             isReadOnly: when(isReadOnly),
