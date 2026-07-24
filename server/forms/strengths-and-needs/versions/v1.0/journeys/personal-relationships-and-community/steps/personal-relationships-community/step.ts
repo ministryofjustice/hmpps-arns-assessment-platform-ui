c8d71e745fcf780e0ea276e8bee1053141d2710f
Iana-Bezrodnykh <iana.bezrodnykh@justice.gov.uk>
2026-07-24 15:55:57 +0100

    Fix redirect to repvent stale data

--- server/forms/strengths-and-needs/versions/v1.0/journeys/personal-relationships-and-community/steps/personal-relationships-community/step.ts
+++ server/forms/strengths-and-needs/versions/v1.0/journeys/personal-relationships-and-community/steps/personal-relationships-community/step.ts
@@ -1,11 +1,24 @@
-import { Condition, Post, redirect, step, submit } from '@ministryofjustice/hmpps-forge/core/authoring'
+import { Answer, Condition, not, or, Post, redirect, step, submit } from '@ministryofjustice/hmpps-forge/core/authoring'
 import { Step } from '../../constants/step'
+import { Question } from '../../constants/question'
+import { Option } from '../../constants/option'
 import { contentFor } from '../../locales'
 import { sectionPath } from '../../../../constants/path'
 import { saveButton } from '../../../../constants/buttons'
 import { StrengthsAndNeedsEffects } from '../../../../../../effects'
 import { Section, SectionStatus } from '../../../../constants/section'
 import { contentBlocks } from './fields'
+
+// allow redirect only if either (makes summary page unreachable unless):
+// > child_parental_responsibilities ticked (then this page would have required parenting responsibilities Q user can't bypass
+// OR
+// > parenting_responsibilities Q doesn't exist on this page (means child_parental_responsibilities wasn't ticked on important_people Q)
+const noStaleParentalResponsibilitiesData = or(
+  Answer(Question.personal_relationships_community_important_people).match(
+    Condition.Array.Contains(Option.child_parental_responsibilities),
+  ),
+  not(Answer(Question.personal_relationships_community_parental_responsibilities).match(Condition.IsRequired())),
+)
 
 export const personalRelationshipsCommunityStep = step({
   path: `/${Step.personal_relationships_community.path}`,
@@ -30,6 +43,7 @@ export const personalRelationshipsCommunityStep = step({
         ],
         next: [
           redirect({
+            when: noStaleParentalResponsibilitiesData,
             goto: Step.personal_relationships_community_summary.path,
           }),
         ],
