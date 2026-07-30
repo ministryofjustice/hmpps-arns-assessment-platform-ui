523c11bd7d22b578b1979c362d65464af36a7c0b
Jake Goodwin <103187669+JakeGoodwin22@users.noreply.github.com>
2026-07-30 15:18:17 +0100

    SP2-2672-2675: Print and share fixes (#659)

--- integration_tests/specs/audit/sentencePlan/printAllGoals.spec.ts
+++ integration_tests/specs/audit/sentencePlan/printAllGoals.spec.ts
@@ -1,4 +1,5 @@
 import { test, TargetService } from '../../../support/fixtures'
+import { currentGoals } from '../../../builders/sentencePlanFactories'
 import PlanOverviewPage from '../../../pages/sentencePlan/planOverviewPage'
 import PrintPreviewPage from '../../../pages/sentencePlan/printPreviewPage'
 import { navigateToSentencePlan } from '../../sentencePlan/sentencePlanUtils'
@@ -8,11 +9,13 @@ test.describe('Print all goals', () => {
   test('accessing print preview from Print all goals sends an audit event', async ({
     page,
     createSession,
+    sentencePlanBuilder,
     auditQueue,
   }) => {
-    const { crn, handoverLink } = await createSession({
+    const { crn, sentencePlanId, handoverLink } = await createSession({
       targetService: TargetService.SENTENCE_PLAN,
     })
+    await sentencePlanBuilder.extend(sentencePlanId).withGoals(currentGoals(1)).save()
 
     await navigateToSentencePlan(page, handoverLink)
     const planOverviewPage = await PlanOverviewPage.verifyOnPage(page)
