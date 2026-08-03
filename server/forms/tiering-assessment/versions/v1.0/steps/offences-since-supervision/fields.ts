0a94353fb05275a47156f27acfcb76808e7b6435
ioannispetridis-hd <ioannis.petridis@hippodigital.co.uk>
2026-08-03 08:25:36 +0100

    ACT-650 - Added radio box with date sub item (#663)

--- server/forms/tiering-assessment/versions/v1.0/steps/offences-since-supervision/fields.ts
+++ server/forms/tiering-assessment/versions/v1.0/steps/offences-since-supervision/fields.ts
@@ -1,16 +1,42 @@
-import { GovUKSummaryList } from '@ministryofjustice/hmpps-forge/govuk-components'
-import { Session } from '@ministryofjustice/hmpps-forge/core/authoring'
+import { GovUKDateInputFull, GovUKRadioInput } from '@ministryofjustice/hmpps-forge/govuk-components'
+import { Condition, Format, Self, validation } from '@ministryofjustice/hmpps-forge/core/authoring'
+import { CaseData } from '../../../../../sentence-plan/versions/v1.0/constants'
 
-export const uuidSummaryField = GovUKSummaryList({
-  card: {
-    title: {
-      text: 'Assessment',
+export const mostRecentOffenceDateField = GovUKDateInputFull({
+  code: 'most-recent-offence-date',
+  fieldset: {
+    legend: {
+      text: Format('What is the date of %1 most recent offence?', CaseData.ForenamePossessive),
+      classes: 'govuk-fieldset__legend--s',
     },
   },
-  rows: [
+  validWhen: [
+    validation({
+      condition: Self().match(Condition.IsRequired()),
+      message: 'Date of most recent offence is a required field',
+    }),
+    validation({
+      condition: Self().match(Condition.Date.IsValid()),
+      message: 'Please enter a valid date',
+    }),
+  ],
+})
+
+export const offenceHistoryField = GovUKRadioInput({
+  code: 'has-commited-offence-since-assessment-date',
+  items: [
     {
-      key: { text: 'UUID' },
-      value: { text: Session('assessmentUuid') },
+      value: 'true',
+      text: 'Yes',
+      block: mostRecentOffenceDateField,
     },
+
+    { value: 'false', text: 'No' },
+  ],
+  validWhen: [
+    validation({
+      condition: Self().match(Condition.IsRequired()),
+      message: 'This is a required field',
+    }),
   ],
 })
