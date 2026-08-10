a7f072e6c47437ec06372917bed2e3e02509d391
OliverYeoman <Ollie.Yeoman@justice.gov.uk>
2026-08-10 11:51:47 +0100

    ACT-666 what drugs did they use page (#688)
    Co-authored-by: “OliverYeoman” <“Ollie.Yeoman@justice.gov.uk”>

--- server/forms/tiering-assessment/versions/v1.0/steps/drug-use/fields.ts
+++ server/forms/tiering-assessment/versions/v1.0/steps/drug-use/fields.ts
@@ -1,16 +1,178 @@
-import { GovUKSummaryList } from '@ministryofjustice/hmpps-forge/govuk-components'
-import { Session } from '@ministryofjustice/hmpps-forge/core/authoring'
+import { and, Answer, Condition, not, Self, validation } from '@ministryofjustice/hmpps-forge/core/authoring'
+import { GovUKCheckboxInput, GovUKRadioInput, GovUKTextInput } from '@ministryofjustice/hmpps-forge/govuk-components'
 
-export const uuidSummaryField = GovUKSummaryList({
-  card: {
-    title: {
-      text: 'Assessment',
+export const drugLastUsedField = (drugValue: string) =>
+  GovUKRadioInput({
+    code: `${drugValue}-radio`,
+    fieldset: {
+      legend: {
+        text: drugValue,
+        classes: 'govuk-visually-hidden',
+      },
+    },
+    dependentWhen: and(
+      Answer('drug-misuse').match(Condition.IsRequired()),
+      Answer('drug-misuse').match(Condition.Array.Contains(drugValue)),
+    ),
+    items: [
+      { value: 'true', text: 'Used in the last 6 months' },
+      { value: 'false', text: 'Used more than 6 months ago' },
+      { divider: 'or' },
+      { value: 'unknow', text: 'Unknown' },
+    ],
+    validWhen: [
+      validation({
+        condition: Self().match(Condition.IsRequired()),
+        message: 'This is a required field',
+      }),
+    ],
+  })
+
+const otherDrugName = GovUKTextInput({
+  code: 'other-drug-name',
+  label: {
+    text: 'other drug name',
+    classes: 'govuk-visually-hidden',
+  },
+  dependentWhen: and(
+    Answer('drug-misuse').match(Condition.IsRequired()),
+    Answer('drug-misuse').match(Condition.Array.Contains('other-drug')),
+  ),
+  validWhen: [
+    validation({
+      condition: Self().match(Condition.IsRequired()),
+      message: 'This is a required field',
+    }),
+    validation({
+      condition: not(Self().not.match(Condition.String.HasMaxLength(200))),
+      message: 'Must be 200 characters or less',
+    }),
+  ],
+})
+
+export const whatDrugsMisusedField = GovUKCheckboxInput({
+  code: 'drug-misuse',
+  multiple: true,
+  fieldset: {
+    legend: {
+      text: 'Which drugs has NAME misused?',
+      classes: 'govuk-fieldset__legend--m',
     },
   },
-  rows: [
+  hint: {
+    text: 'Select all that apply.',
+  },
+  items: [
     {
-      key: { text: 'UUID' },
-      value: { text: Session('assessmentUuid') },
+      value: 'amphetamines',
+      text: 'Amphetamines',
+      block: drugLastUsedField('amphetamines'),
     },
+    {
+      value: 'benzodiazepines',
+      text: 'Benzodiazepines',
+      block: drugLastUsedField('benzodiazepines'),
+
+    },
+    {
+      value: 'cannabis',
+      text: 'Cannabis',
+      block: drugLastUsedField('cannabis'),
+
+    },
+    {
+      value: 'cocaine-hydrochloride',
+      text: 'Cocaine hydrochloride',
+      block: drugLastUsedField('cocaine-hydrochloride'),
+    },
+    {
+      value: 'crack-or-cocaine',
+      text: 'Crack or cocaine',
+      block: drugLastUsedField('crack-or-cocaine'),
+    },
+    {
+      value: 'ecstasy',
+      text: 'Ecstasy (also known as MDMA)',
+      block: drugLastUsedField('ecstasy'),
+    },
+    {
+      value: 'hallucinogens',
+      text: 'Hallucinogens',
+      block: drugLastUsedField('hallucinogens'),
+    },
+    {
+      value: 'heroin',
+      text: 'Heroin',
+      block: drugLastUsedField('heroin'),
+    },
+    {
+      value: 'ketamine',
+      text: 'Ketamine',
+      block: drugLastUsedField('ketamine'),
+    },
+    {
+      value: 'methadone',
+      text: 'Methadone (not prescribed)',
+      block: drugLastUsedField('methadone'),
+    },
+    {
+      value: 'misused-prescribed-drugs',
+      text: 'Misused prescribed drugs',
+      block: drugLastUsedField('misused-prescribed-drugs'),
+    },
+    {
+      value: 'other-opiates',
+      text: 'Other opiates',
+      block: drugLastUsedField('other-opiates'),
+    },
+    {
+      value: 'solvents',
+      text: 'Solvents (including gases and glues)',
+      block: drugLastUsedField('solvents'),
+    },
+    {
+      value: 'spice',
+      text: 'Spice',
+      block: drugLastUsedField('spice'),
+    },
+    {
+      value: 'steroids',
+      text: 'Steroids',
+      block: drugLastUsedField('steroids'),
+    },
+    {
+      value: 'other-drug',
+      text: 'Other',
+      block: [otherDrugName, drugLastUsedField('other-drug')],
+    },
+  ],
+  validWhen: [
+    validation({
+      condition: Self().match(Condition.IsRequired()),
+      message: 'This is a required field',
+    }),
+  ],
+})
+
+export const motivationToStopMisuseField = GovUKRadioInput({
+  code: 'motivation-to-tackle-drug-misuse',
+  fieldset: {
+    legend: {
+      text: 'Does NAME seem motivated to stop or reduce their drug use?',
+      classes: 'govuk-fieldset__legend--s',
+    },
+  },
+  items: [
+    { value: 'NO_MOTIVATION', text: 'Does not show motivation to stop or reduce' },
+    { value: 'PARTIAL_MOTIVATION', text: 'Shows some motivation to stop or reduce' },
+    { value: 'FULL_MOTIVATION', text: 'Motivated to stop or reduce' },
+    { divider: 'or' },
+    { value: 'unknow', text: 'Unknown' },
+  ],
+  validWhen: [
+    validation({
+      condition: Self().match(Condition.IsRequired()),
+      message: 'This is a required field',
+    }),
   ],
 })
