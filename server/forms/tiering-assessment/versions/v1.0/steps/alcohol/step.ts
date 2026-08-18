da4a0d27060a57af592a0d6207b16c8ef96b3866
Thomas Bedford <thomas.bedford@hippodigital.co.uk>
2026-08-18 10:45:22 +0100

    Linting fix

--- server/forms/tiering-assessment/versions/v1.0/steps/alcohol/step.ts
+++ server/forms/tiering-assessment/versions/v1.0/steps/alcohol/step.ts
@@ -8,10 +8,7 @@ export const alcoholStep = step({
   title: 'Alcohol',
   onAccess: [
     access({
-      effects: [
-        TieringAssessmentEffects.LoadAssessmentData(),
-        TieringAssessmentEffects.LoadForename()
-      ],
+      effects: [TieringAssessmentEffects.LoadAssessmentData(), TieringAssessmentEffects.LoadForename()],
     }),
   ],
   blocks: [currentAlcoholUseFrequencyField, unitsOfAlcoholField, alcoholUnitsTable, bingeDrinkingField, continueButton],
