e15229b2e3bed5ecbc67131fa3ae3e290226f6c6
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2026-08-20 14:47:55 +0100

    SP2-2582: Telemetry tags added to dropdown menus with minor logic change for seperation of concerns between "Clear" and "Remove" step link.

--- server/forms/sentence-plan/versions/v1.0/journeys/goal-management/add-steps/fields.ts
+++ server/forms/sentence-plan/versions/v1.0/journeys/goal-management/add-steps/fields.ts
@@ -86,6 +86,8 @@ const areaBlockContent = when(Data('activeGoal.relatedAreasOfNeedLabels').match(
 
 const goalBlockContent = Format('<p>Goal: %1</p>', Data('activeGoal.title').pipe(Transformer.String.EscapeHtml()))
 
+const isSingleStep = Data('activeGoalStepsEdited').pipe(Transformer.Array.Length()).match(Condition.Equals(1))
+
 export const goalContextInsetText = GovUKInsetText({
   classes: 'govuk-!-margin-top-2',
   blocks: [HtmlBlock({ content: areaBlockContent }), HtmlBlock({ content: goalBlockContent })],
@@ -178,6 +180,9 @@ export const stepRows = HtmlBlock({
                   describedBy: stepActorHintId,
                   items: actorLabelOptions,
                   defaultValue: Item().path('actor'),
+                  attributes: {
+                    'data-ai-id': Format('add-steps-step-actor-dropdown-%1', Loop.Index0()),
+                  },
                   validWhen: [
                     validation({
                       condition: Self().match(Condition.IsRequired()),
@@ -227,6 +232,9 @@ export const stepRows = HtmlBlock({
                   describedBy: stepStatusHintId,
                   items: stepStatusOptions,
                   defaultValue: Item().path('status'),
+                  attributes: {
+                    'data-ai-id': Format('add-steps-step-status-dropdown-%1', Loop.Index0()),
+                  },
                   validWhen: [
                     validation({
                       condition: Self().match(Condition.IsRequired()),
@@ -241,12 +249,15 @@ export const stepRows = HtmlBlock({
             width: 'one-sixth',
             blocks: [
               ButtonAsLink({
-                text: when(Data('activeGoalStepsEdited').pipe(Transformer.Array.Length()).match(Condition.Equals(1)))
-                  .then('Clear')
-                  .else('Remove'),
+                text: when(isSingleStep).then('Clear').else('Remove'),
                 name: 'action',
                 value: Format('remove_%1', Loop.Index0()),
                 classes: 'govuk-!-margin-bottom-0',
+                attributes: {
+                  'data-ai-id': when(isSingleStep)
+                    .then('add-steps-clear-step-link')
+                    .else(Format('add-steps-remove-step-link-%1', Loop.Index0())),
+                },
               }),
             ],
           },
