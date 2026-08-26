ecfe5a20cdaba83df4c2244cf4d7264d2d2c60df
Ioannis Petridis <ioannis.petridis@hippodigital.co.uk>
2026-08-26 10:29:13 +0100

    Lint fix

--- server/forms/tiering-assessment/versions/v1.0/index.ts
+++ server/forms/tiering-assessment/versions/v1.0/index.ts
@@ -18,7 +18,7 @@ import { thinkingAttitudesAndBehavioursStep } from './steps/thinking-attitudes-a
 import { offenceAnalysisStep } from './steps/offence-analysis/step'
 import { previousConvictionsStep } from './steps/previous-convictions/step'
 import { accommodationStep } from './steps/accommodation-old/step'
-//import { accommodationStep } from './steps/accommodation/step'
+// import { accommodationStep } from './steps/accommodation/step'
 import { checkYourAnswersStep } from './steps/check-your-answers-old/step'
 // import { checkYourAnswersStep } from './steps/check-your-answers/step'
 
