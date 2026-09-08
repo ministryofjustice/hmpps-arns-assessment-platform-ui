d8a4b72625c753da7d0e9c71b110721fd5b69431
Martin <martin.georgiev@justice.gov.uk>
2026-09-08 14:11:19 +0100

    ARN-3177 - lint fix

--- server/forms/strengths-and-needs/versions/v1.0/journeys/accommodation/section.ts
+++ server/forms/strengths-and-needs/versions/v1.0/journeys/accommodation/section.ts
@@ -294,7 +294,7 @@ const livingWith = question({
     field: checkboxField({ dependentWhen: livingWithApplies, visibleWhen: livingWithApplies }),
     summaryRow: itemisedSummaryRow({
       changePath: Step.accommodation_details.path,
-      visibleWhen: livingWithApplies
+      visibleWhen: livingWithApplies,
     }),
   },
 })
