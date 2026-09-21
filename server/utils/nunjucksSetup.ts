98a3dc9b8108723e2bb18e65b1249a75b2f98edf
Aiden Herbert <Aiden.Herbert@justice.gov.uk>
2026-09-21 15:01:23 +0100

    SP2-2329: Addition of RegEx to replace `_` with `-` for error codes applied to telemetry tags

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -127,7 +127,7 @@ export default function nunjucksSetup(app?: express.Express) {
       text: error.message,
       href: (error.details?.href as string | undefined) ?? (error.blockCode ? `#${error.blockCode}` : ''),
       attributes: {
-        'data-ai-id': `error-summary-link-${error.blockCode}`,
+        'data-ai-id': `error-summary-link-${error.blockCode.replace(/_/g, '-')}`,
       },
     })),
   )
