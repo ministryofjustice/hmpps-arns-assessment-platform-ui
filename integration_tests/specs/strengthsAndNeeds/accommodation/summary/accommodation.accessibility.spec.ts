2399d2a3ad900f88cba4c61c1de4a5f5c26546eb
lewis prescott <lewis.prescott@justice.gov.uk>
2026-09-21 16:14:04 +0100

    test: const questions and options

--- integration_tests/specs/strengthsAndNeeds/accommodation/summary/accommodation.accessibility.spec.ts
+++ integration_tests/specs/strengthsAndNeeds/accommodation/summary/accommodation.accessibility.spec.ts
@@ -1,6 +1,9 @@
 import AccommodationPage from 'pages/strengthsAndNeeds/accommodationPage'
-import { test, TargetService } from '../../../../support/fixtures'
+import { Question } from '@server/forms/strengths-and-needs/versions/v1.0/journeys/accommodation/constants/question'
+import { Option } from '@server/forms/strengths-and-needs/versions/v1.0/journeys/accommodation/constants/option'
+import { CommonOption } from '@server/forms/strengths-and-needs/versions/v1.0/constants/commonOption'
 import { checkAccessibility, navigateToStrengthsAndNeeds } from '../../sanUtils'
+import { test, TargetService } from '../../../../support/fixtures'
 
 test.describe('Accessibility', () => {
   test('should be accessible', async ({ page, createSession, strengthsAndNeedsBuilder }) => {
@@ -9,14 +12,14 @@ test.describe('Accessibility', () => {
     })
     await strengthsAndNeedsBuilder
       .extend(sanAssessmentId).withAnswers([
-        { question: 'current_accommodation', value: 'SETTLED' },
-        { question: 'type_of_settled_accommodation', value: 'HOMEOWNER' },
-        { question: 'living_with', value: ['FAMILY'] },
-        { question: 'suitable_housing_location', value: 'NO' },
-        { question: 'suitable_housing_location_concerns', value: [] },
-        { question: 'suitable_housing', value: 'NO' },
-        { question: 'unsuitable_housing_concerns', value: [] },
-        { question: 'accommodation_changes', value: 'NOT_PRESENT' },
+        { question: Question.current_accommodation, value: Option.settled },
+        { question: Question.type_of_settled_accommodation, value: Option.homeowner },
+        { question: Question.living_with, value: [Option.family] },
+        { question: Question.suitable_housing_location, value: CommonOption.no },
+        { question: Question.suitable_housing_location_concerns, value: [] },
+        { question: Question.suitable_housing, value: CommonOption.no },
+        { question: Question.unsuitable_housing_concerns, value: [] },
+        { question: Question.accommodation_changes, value: CommonOption.not_present },
       ]).save()
 
     await navigateToStrengthsAndNeeds(page, handoverLink, 'accommodation-summary')
