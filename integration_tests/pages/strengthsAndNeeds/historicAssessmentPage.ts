0334a09774d9bcbe0cbd24d240528b87f006c05c
Kaustav Surai <kaustav.surai@justice.gov.uk>
2026-09-22 22:15:17 +0100

    removed unused import

--- integration_tests/pages/strengthsAndNeeds/historicAssessmentPage.ts
+++ integration_tests/pages/strengthsAndNeeds/historicAssessmentPage.ts
@@ -1,4 +1,4 @@
-import { expect, type Locator, type Page } from '@playwright/test'
+import { type Locator, type Page } from '@playwright/test'
 import StrengthsAndNeedsPage from './strengthsAndNeedsPage'
 
 export default class HistoricAssessmentPage extends StrengthsAndNeedsPage {
