a5580896c2ca5f83f496fc5812ba028f3cdc541a
Kaustav Surai <kaustav.surai@justice.gov.uk>
2026-09-25 13:46:28 +0100

    covered hidden sections

--- integration_tests/specs/strengthsAndNeeds/alcoholUse/alcoholUse.questions.spec.ts
+++ integration_tests/specs/strengthsAndNeeds/alcoholUse/alcoholUse.questions.spec.ts
@@ -44,10 +44,12 @@ test.describe('Questions', () => {
     await strengthsAndNeedsBuilder
       .extend(sanAssessmentId)
       .withAnswers([{ question: Question.alcohol_use, value: Option.yes_within_last_three_months }])
+      .withAnswers([{ question: Question.alcohol_binge_drinking, value: 'YES' }])
       .save()
 
     await AlcoholUsePage.navigateTo(page, handoverLink, baseURL, sanAssessmentId, 'alcohol-use-details')
     const alcoholUsePage = await AlcoholUsePage.verifyOnPage(page, 'Alcohol use')
+    await page.getByText('Help with alcohol units').click()
 
     await expect(alcoholUsePage.mainForm).toMatchAriaSnapshot(`
       - group "How often has Test drunk alcohol in the last 3 months?":
@@ -62,7 +64,41 @@ test.describe('Questions', () => {
         - text: More than 4 times a week
       - group "How many units of alcohol does Test have on a typical day of drinking?":
         - text: How many units of alcohol does Test have on a typical day of drinking?
-        - group: Help with alcohol units
+        - group:
+          - text: Help with alcohol units
+          - table:
+            - rowgroup:
+              - row "Type of drink Number of alcohol units":
+                - columnheader "Type of drink"
+                - columnheader "Number of alcohol units"
+            - rowgroup:
+              - row "Single small shot of spirits (25ml, ABV 40%) For example, whisky or vodka. 1 unit":
+                - cell "Single small shot of spirits (25ml, ABV 40%) For example, whisky or vodka."
+                - cell "1 unit"
+              - row "Alcopop (275ml, ABV 5.5%) 1.5 units":
+                - cell "Alcopop (275ml, ABV 5.5%)"
+                - cell "1.5 units"
+              - row "Small glass of red/white/rosé wine (125ml, ABV 12%) 1.5 units":
+                - cell "Small glass of red/white/rosé wine (125ml, ABV 12%)"
+                - cell "1.5 units"
+              - row "Bottle of lager/beer/cider (330ml, ABV 5%) 1.7 units":
+                - cell "Bottle of lager/beer/cider (330ml, ABV 5%)"
+                - cell "1.7 units"
+              - row "Can of lager/beer/cider (440ml, ABV 5.5%) 2.4 units":
+                - cell "Can of lager/beer/cider (440ml, ABV 5.5%)"
+                - cell "2.4 units"
+              - row "Pint of lower-strength lager/ beer/cider (ABV 3.6%) 2 units":
+                - cell "Pint of lower-strength lager/ beer/cider (ABV 3.6%)"
+                - cell "2 units"
+              - row "Standard glass of red/white/rosé wine (175ml, ABV 12%) 2.1 units":
+                - cell "Standard glass of red/white/rosé wine (175ml, ABV 12%)"
+                - cell "2.1 units"
+              - row "Pint of higher-strength lager/ beer/cider (ABV 5.2%) 3 units":
+                - cell "Pint of higher-strength lager/ beer/cider (ABV 5.2%)"
+                - cell "3 units"
+              - row "Large glass of red/white/rosé wine (250ml, ABV 12%) 3 units":
+                - cell "Large glass of red/white/rosé wine (250ml, ABV 12%)"
+                - cell "3 units"
         - radio "1 to 2 units"
         - text: 1 to 2 units
         - radio "3 to 4 units"
@@ -75,8 +111,18 @@ test.describe('Questions', () => {
         - text: 10 or more units
       - group "Has Test had 8 or more units within a single day of drinking in the last 3 months?":
         - text: Has Test had 8 or more units within a single day of drinking in the last 3 months?
-        - radio "Yes"
+        - radio "Yes" [checked]
         - text: "Yes"
+        - group "Select how often":
+          - text: Select how often
+          - radio "Less than a month"
+          - text: Less than a month
+          - radio "Monthly"
+          - text: Monthly
+          - radio "Weekly"
+          - text: Weekly
+          - radio "Daily or almost daily"
+          - text: Daily or almost daily
         - radio "No"
         - text: "No"
       - group "Has Test shown evidence of binge drinking or excessive alcohol use in the last 6 months?":
